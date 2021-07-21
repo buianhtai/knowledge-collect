e712ed6fc76219046d1b03ec65ab1850dac87cb8
Tai.Bui <buianhtai90@gmail.com>
2021-07-21 14:38:00 +0700

    Create bestSum.java

---
+++ bestSum.java
@@ -0,0 +1,23 @@
+Given an array --> return the minimun 
+
+    public static void main(String[] args) {
+        System.out.println(bestSum(new int[]{5,3,4,7}, 7)); //--> [7]
+        System.out.println(bestSum(new int[]{2,3,5}, 8));// ---> [3,5]
+        System.out.println(bestSum(new int[]{1,4,5}, 8)); //---> [4,4]
+    }
+public static List<Integer> bestSum(int[] numbers, int target) {
+        if (target == 0) return new ArrayList<>();
+        if (target < 0) return null;
+        List<Integer> shortestSum = null;
+        for (int number : numbers) {
+            int remainder = target - number;
+            List<Integer> remainderCombination = bestSum(numbers, remainder);
+            if (remainderCombination != null) {
+                remainderCombination.add(number);
+                if (shortestSum == null || remainderCombination.size() < shortestSum.size()) {
+                    shortestSum= remainderCombination;
+                }
+            }
+        }
+        return shortestSum;
+    }
